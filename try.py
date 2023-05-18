b82cfdba1e5dbcb10369436457b61fd4528a7ee3
AdhinJoseph <133904417+AdhinJoseph@users.noreply.github.com>
2023-05-18 15:49:53 +0400

    try.py

--- try.py
+++ try.py
@@ -19,39 +19,54 @@ if a.lower()=="yes":
     y=random.choice(c)
     z=random.choice(b)
     k=list(v)
+    print(k)
     r=[]
     if i.lower()=="movies":
-        print (v)
         print("Your word is so length in a programmers way ,starting from 0 :) is :",len(k)-1)
         print(k)
+        print (v)
+        s1=len(k)
+        k1='*'*s1
+        r=list(k1)
+        m1=check(k,' ')
+        m2=check(k,'-')
+        if m1!=[]:
+            for i in m1:
+                r.pop(i)
+                r.insert(i,' ')
+        if m2!=[]:
+            for i in m2:
+                r.pop(i)
+                r.insert(i,'-')
         x1=check(k,'a')
         x2=check(k,'e')
         x3=check(k,'i')
         x4=check(k,'o')
         x5=check(k,'u')
-        print(x1,x2,x3,x4,x5)
         if x1 !=[]:
-            print("The word contains the letter 'a' with its position being at ",x1)
-        else:
-            pass
+            for i in x1:
+                r.pop(i)
+                r.insert(i,'a')
         if x2 !=[]:
-            print("The word contains the letter 'e' with its position being at ",x2)
-        else:
-            pass
+            for i in x2:
+                r.pop(i)
+                r.insert(i,'e')
         if x3 !=[]:
-            print("The word contains the letter 'i' with its position being at ",x3)
-        else:
-            pass
+            for i in x3:
+                r.pop(i)
+                r.insert(i,'i')
         if x4 !=[]:
-            print("The word contains the letter 'o' with its position being at ",x4)
-        else:
-            pass
+            for i in x2:
+                r.pop(i)
+                r.insert(i,'o')
         if x5 !=[]:
-            print("The word contains the letter 'u' with its position being at ",x5)
-        else:
-            pass
-        
-        
+            for i in x2:
+                r.pop(i)
+                r.insert(i,'u')
+        print(r)
+        print("Space seen between *'s indicate a space in orginal sentence")
+        for i in r:
+            print(i,end=" ")
         
     elif i.lower()=="games":
         print (w)
